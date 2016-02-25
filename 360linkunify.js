9f260f846d3ee1ae5d83eb2855ac88b35b2b900f
wonox <ono.wataru.plus@gmai.com>
2016-02-26 08:33:30 +0900

    code refine

--- 360linkunify.js
+++ 360linkunify.js
@@ -1,69 +1,7 @@
-function parsedom(res, name, namae, baseurl) {
-	var dpObj = new DOMParser();
-	var xmlText = res["responseText"];
-	var xmlDoc = $.parseXML(xmlText);
-	console.log(res);
-	var xmlcontent=res["responseText"];
-	console.log(content+xmlcontent);
-	var listdata="";
-	// 両方をカンマ区切りで指定してブラウザによる差異を吸収。
-	// toDo: <ssopenurl:linkGroup type="holding">がなければスルー
-	$(xmlDoc).find("ssopenurl\\:result, result").each(function(){
-
-		var title=$(this).find("dc\\:title, title").text();
-		var source=$(this).find("dc\\:source, source").text();
-		var issn=$(this).find("ssopenurl\\:issn, issn").text();
-		var eissn=$(this).find("ssopenurl\\:eissn, eissn").text();
-		var volume=$(this).find("ssopenurl\\:volume, volume").text();
-		var issue=$(this).find("ssopenurl\\:issue, issue").text();
-		var spage=$(this).find("ssopenurl\\:spage, spage").text();
-		var startDate=$(this).find("ssopenurl\\:startDate, startDate").text();
-		var providerName=$(this).find("ssopenurl\\:providerName, providerName").text();
-		var databaseName=$(this).find("ssopenurl\\:databaseName, databaseName").text();
-		var startDate=$(this).find("ssopenurl\\:startDate, startDate").text();
-		var endDate=$(this).find("ssopenurl\\:endDate, endDate").text();
-		//var surl=$(this).find("ssopenurl\\:url").text();
-		var jurl=$(this).find("ssopenurl\\:url[type='journal'], url[type='journal']").text();
-		var aurl=$(this).find("ssopenurl\\:url[type='article'], url[type='article']").text();
-		var vurl=$(this).find("ssopenurl\\:url[type='volume'], url[type='volume']").text();
-		var turl=$(this).find("ssopenurl\\:url[type='termsOfUse'], url[type='termsOfUse']").text();
-
-		if ($(this).find("ssopenurl\\:linkGroups, linkGroups").text()) {
-			// listdata+="<li><h4>"+title+"</h4><p>source: "+ source + " issn: " + issn + " eissn: " + eissn + " vol: " + volume + " issue: " + issue + " spage: " + spage + " startDate: " + startDate + " endDate: " + endDate + " providerName: " + providerName + " databaseName: " + databaseName + '</p><p><a href="' + vurl + '">' + vurl +'</a><br/><a href="' + jurl + '">' + jurl +'</a><br/><a href="' + aurl + '">' + aurl + '</a><br/><a href="' + turl + '">termsOfUse: ' + turl + '</a></p></li>';
-			listdata+='<li><h4>'+title+'</h4><p>source: ' + source + "<br /> issn: " + issn + " eissn: " + eissn + " vol: " + volume + " issue: " + issue + " spage: " + spage + " startDate: " + startDate + " endDate: " + endDate + " providerName: " + providerName + " databaseName: " + databaseName + '</p><p><a href="' + vurl + '">' + vurl +'</a><br/><a href="' + jurl + '">' + jurl +'</a><br/><a href="' + aurl + '">' + aurl + '</a>';
-			if (turl) { listdata += '<br/><a href="' + turl + '">termsOfUse: ' + turl + '</a></p></li>' }
-				else { listdata += '</p></li>'};
-		};
-	});
-
- 	//	$(".hyouji2").html("<h4>ここにテキストデータを変換したXMLがでます</h4>"+listdata);
-	// var name = '.' + name;
-	var resolver = baseurl.replace(/\.openurl\.xml\.serialssolutions\.com\/openurlxml/, ".search.serialssolutions.com/");
-	// alert(resolver);
-	if ( listdata == "" ) listdata = "nodata";
-	// $(name).html("<h3><a href=\"" + resolver + "\">"+namae+"</a></h3>"+listdata);
-	$("#resolver").append('<br />' + '<h3 lang="ja"><a href=\"' + resolver + "\">"+namae+'</a></h3><h3 lang="en"><a href=\"' + resolver + "\">"+name+'</a></h3><ol lang="other">'+listdata + "</ol>");
-}
-
-function ajaxxml(baseurl,name,namae) {
-//Javascript変数定義
-//var content ="";
-	$(document).ready(function(){
-		$.ajax({
-			url: baseurl,
-			type: "GET",
-			success: function(res) {
- 				parsedom(res, name, namae, baseurl);
- 			},
-			fail: function() {
-				alert(fail);
-			}
-		});
-	});
-}
-
-/*base URL of other libraries*/
+/*base URL of libraries' 360Core*/
 var portal=[
+{"base":"http://te7fv6dm8k","name":'University of Texas at Austin',"namae":' University of Texas at Austin'},
+{"base":"http://wa4py6yj8t","name":'Yale University Library ',"namae":' Yale University Library'},
 {"base":"http://ck3cc4bu9u","name":'doshisha univ.',"namae":'同志社大学'},
 {"base":"http://mx9kp2xn4f","name":'chuo univ.',"namae":'中央大学'},
 {'base':'http://tm3xa4ur3u','name':'waseda univ.','namae':'早稲田大学'},
@@ -96,15 +34,73 @@ var portal=[
 {'base':'http://kx3ry9kp2c','name':'tokyo noko univ.','namae':'東京農工大学'},
 {'base':'http://qq6az3es8a','name':'kyoto kogei univ.','namae':'京都工芸繊維大学'},
 {'base':'http://yj3bg6ng2l','name':'hokuriku univ.','namae':'北陸先端科学技術大学院大学'}
-  ];
+];
+
+function parsedom(res, name, namae, baseurl) {
+	var dpObj = new DOMParser();
+	var xmlText = res["responseText"];
+	var xmlDoc = $.parseXML(xmlText);
+	// console.log(res);
+	var xmlcontent=res["responseText"];
+	// console.log(content+xmlcontent);
+	var listdata="";
+	// Absorb the difference by the browser by specifying both separated by commas
+	// toDo: if no <ssopenurl:linkGroup type="holding"> through out
+	$(xmlDoc).find("ssopenurl\\:result, result").each(function(){
+
+		var title=$(this).find("dc\\:title, title").text();
+		var source=$(this).find("dc\\:source, source").text();
+		var issn=$(this).find("ssopenurl\\:issn, issn").text();
+		var eissn=$(this).find("ssopenurl\\:eissn, eissn").text();
+		var volume=$(this).find("ssopenurl\\:volume, volume").text();
+		var issue=$(this).find("ssopenurl\\:issue, issue").text();
+		var spage=$(this).find("ssopenurl\\:spage, spage").text();
+		var startDate=$(this).find("ssopenurl\\:startDate, startDate").text();
+		var providerName=$(this).find("ssopenurl\\:providerName, providerName").text();
+		var databaseName=$(this).find("ssopenurl\\:databaseName, databaseName").text();
+		var startDate=$(this).find("ssopenurl\\:startDate, startDate").text();
+		var endDate=$(this).find("ssopenurl\\:endDate, endDate").text();
+		//var surl=$(this).find("ssopenurl\\:url").text();
+		var jurl=$(this).find("ssopenurl\\:url[type='journal'], url[type='journal']").text();
+		var aurl=$(this).find("ssopenurl\\:url[type='article'], url[type='article']").text();
+		var vurl=$(this).find("ssopenurl\\:url[type='volume'], url[type='volume']").text();
+		var turl=$(this).find("ssopenurl\\:url[type='termsOfUse'], url[type='termsOfUse']").text();
+
+		if ($(this).find("ssopenurl\\:linkGroups, linkGroups").text()) {
+			listdata+='<li><h4>'+title+'</h4><p>source: ' + source + "<br /> issn: " + issn + " eissn: " + eissn + " vol: " + volume + " issue: " + issue + " spage: " + spage + " startDate: " + startDate + " endDate: " + endDate + " providerName: " + providerName + " databaseName: " + databaseName + '</p><p><a href="' + vurl + '">' + vurl +'</a><br/><a href="' + jurl + '">' + jurl +'</a><br/><a href="' + aurl + '">' + aurl + '</a>';
+			if (turl) { listdata += '<br/><a href="' + turl + '">termsOfUse: ' + turl + '</a></p></li>' }
+				else { listdata += '</p></li>'};
+		};
+	});
+
+	var resolver = baseurl.replace(/\.openurl\.xml\.serialssolutions\.com\/openurlxml/, ".search.serialssolutions.com/");
+	if ( listdata == "" ) listdata = "nodata";
+	$("#resolver").append('<br />' + '<h3 lang="ja"><a href=\"' + resolver + "\">"+namae+'</a></h3><h3 lang="en"><a href=\"' + resolver + "\">"+name+'</a></h3><ol lang="other">'+listdata + "</ol>");
+}
+
+function ajaxxml(baseurl,name,namae) {
+	$(document).ready(function(){
+		$.ajax({
+			url: baseurl,
+			type: "GET",
+			success: function(res) {
+ 				parsedom(res, name, namae, baseurl);
+ 			},
+			fail: function() {
+				alert(fail);
+			}
+		});
+	});
+}
 
 // main
 var opurl =	window.location.search;
 if (opurl.match(/Z39\.88/)) {
   if ( !opurl.match(/version=1.0/)) opurl = opurl.replace(/(\?)/g, "$1version=1.0&" );
   opurl = opurl.replace("url_ver=Z39.88-2003&", "" );
+	alert(opurl);
   main(opurl);
-} else {alert("failure")}
+} else {alert("Reload by applying a query or submit by filling in the ISSN to form.")}
 
 function main(opurl) {
   for(i in portal) {
@@ -112,15 +108,15 @@ function main(opurl) {
   }
 }
 
-// フォームの値を取得する
+// get the value of the form
 function my1() {
-	// var keyname = document.getElementById('my-text').value;
-	var keyname = $('#my-form [name=my-text]').val(); // jqueryを使った場合
+	// var keyname = document.getElementById('my-text').value; // no jquery
+	var keyname = $('#my-form [name=my-text]').val(); // jquery
 	removeall("resolver");
 	if (keyname == '') {removeall("result");};
 	if (!keyname.match(/[0-9\-xX]{8}/)) {
 		alert("input only valid ISSN");
-		return false; // break の代わり
+		return false; // break
 		};
 	keyname = '?version=1.0&ctx_ver=Z39.88-2004&ctx_enc=info:ofi/enc:UTF-8&rfr_id=info:sid/summon.serialssolutions.com&rft_val_fmt=info:ofi/fmt:kev:mtx:journal&rft.genre=article&rft.issn=' + keyname;
 	main(keyname);
@@ -128,14 +124,11 @@ function my1() {
 
 function removeall (id){
 	var aNode = document.getElementById(id);
-	// aNode.parentNode.removeChild(aNode);
-
 	for (var i =aNode.childNodes.length-1; i>=0; i--) {
 		aNode.removeChild(aNode.childNodes[i]);
 	}
 }
 
-// http://mx9kp2xn4f.search.serialssolutions.com/
 // sample_url: 'http://vs2ga4mq9g.openurl.xml.serialssolutions.com/openurlxml?version=1.0&ctx_ver=Z39.88-2004&ctx_enc=info:ofi/enc:UTF-8&rfr_id=info:sid/summon.serialssolutions.com&rft_val_fmt=info:ofi/fmt:kev:mtx:journal&rft.genre=article&rft.atitle=Iodine+status+of+UK+schoolgirls:+a+cross-sectional+survey&rft.jtitle=The+Lancet&rft.au=Vanderpump,+Mark+PJ&rft.au=Holder,+Roger+L&rft.au=Lazarus,+John+H&rft.au=Boelaert,+Kristien&rft.au=Laurberg,+Peter&rft.au=Smyth,+Peter+P&rft.au=Franklyn,+Jayne+A&rft.date=2011-06-17&rft.pub=Elsevier+B.V&rft.issn=0140-6736&rft.volume=377&rft.issue=9782&rft.spage=2007&rft.epage=2012&rft_id=info:doi/10.1016/S0140-6736%2811%2960693-4&rft.externalDBID=GLAN&rft.externalDocID=10_1016_S0140_6736_11_60693_4',
 // opurl = "?version=1.0&ctx_ver=Z39.88-2004&ctx_enc=info:ofi/enc:UTF-8&rfr_id=info:sid/summon.serialssolutions.com&rft_val_fmt=info:ofi/fmt:kev:mtx:journal&rft.genre=article&rft.atitle=Iodine+status+of+UK+schoolgirls:+a+cross-sectional+survey&rft.jtitle=The+Lancet&rft.au=Vanderpump,+Mark+PJ&rft.au=Holder,+Roger+L&rft.au=Lazarus,+John+H&rft.au=Boelaert,+Kristien&rft.au=Laurberg,+Peter&rft.au=Smyth,+Peter+P&rft.au=Franklyn,+Jayne+A&rft.date=2011-06-17&rft.pub=Elsevier+B.V&rft.issn=0140-6736&rft.volume=377&rft.issue=9782&rft.spage=2007&rft.epage=2012&rft_id=info:doi/10.1016/S0140-6736%2811%2960693-4&rft.externalDBID=GLAN&rft.externalDocID=10_1016_S0140_6736_11_60693_4";
 // opurl = "?version=1.0&ctx_ver=Z39.88-2004&ctx_enc=info:ofi/enc:UTF-8&rfr_id=info:sid/summon.serialssolutions.com&rft_val_fmt=info:ofi/fmt:kev:mtx:journal&rft.genre=journal&rfr_dat=SB00050365&rft.issn=00014273&rft.jtitle=The+Academy+of+Management+journal";
